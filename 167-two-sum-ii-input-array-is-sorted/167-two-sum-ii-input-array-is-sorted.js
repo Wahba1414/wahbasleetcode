006a3901535dcd36db5483f7ea1cf123db240a96
Wahba1414 <42750080+Wahba1414@users.noreply.github.com>
2022-06-09 17:26:31 +0200

    Time: 95 ms (37.13%), Space: 42.6 MB (87.89%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    var sum = 0;
+    var left = 0;
+    var right = numbers.length - 1;
+  
+    while (left < right) {
+        sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum > target) {
+            right--;
+        } else {
+            left++;
+        }
+  }
+};
